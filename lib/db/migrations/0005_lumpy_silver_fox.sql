e3296066cfa86f0d9279d3f41938c07c47c6cd23
Akuya Ekorot <akuyaekorot@gmail.com>
2024-01-28 20:34:54 +0300

    push changes to database

---
+++ lib/db/migrations/0005_lumpy_silver_fox.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS "collection_products" (
+	"id" varchar(191) PRIMARY KEY NOT NULL,
+	"collection_id" varchar(256) NOT NULL,
+	"product_id" varchar(256) NOT NULL,
+	"user_id" varchar(256) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "collection_products" ADD CONSTRAINT "collection_products_collection_id_collections_id_fk" FOREIGN KEY ("collection_id") REFERENCES "collections"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "collection_products" ADD CONSTRAINT "collection_products_product_id_products_id_fk" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "collection_products" ADD CONSTRAINT "collection_products_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
